2b5afb52d099b97e1157fb943a9fc66811833d54
fbwougr121 <fbwogur121@naver.com>
2024-03-18 11:08:42 +0900

    Initial commit

---
+++ src/main/java/com/Tmax/CodeSummary/main/codeSummary.java
@@ -0,0 +1,12 @@
+package com.Tmax.CodeSummary.main;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class codeSummary  extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e){
+
+    }
+}
